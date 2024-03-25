2df91734684d4a88f114d6d783465b35514836ea
Celine Tran <celinephoungtran@gmail.com>
2024-03-25 16:12:52 -0400

    redo validation

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -1,3 +1,7 @@
+let year, month, day;
+let feb;
+let validDays
+
 const textYear = document.getElementById("years");
 const textMonths = document.getElementById("months");
 const textDays = document.getElementById("days");
@@ -6,59 +10,75 @@ const inputYear = document.getElementById("year-input");
 const inputMonth = document.getElementById("month-input");
 const inputDay = document.getElementById("day-input");
 
-const labelYear = document.getElementById("year-label");
-const labelMonth = document.getElementById("month-label");
-const labelDay = document.getElementById("day-label");
-
-function validateValueRange(value, minValue, maxValue) {
-    if (value < minValue || value > maxValue) {
-        return false;
-    }
-    return true;
-}
-
-function validateDays(day, month, validDays) {
-    if (day > validDays[month]) {
-        return false;
-    } 
-    return true;
-}
-
-function validateIsInPast(day, month, year) {
-    let currDate = new Date();
-    let inputDate = new Date(year, month, day);
-    return currDate > inputDate;
-}
-
-function applyInputError(labelElement, inputElement, isValid) {
+function applyInputError(inputElement, labelElement, errorElement, isValid) {
     if (isValid) {
         labelElement.classList.remove("text-error");
         inputElement.classList.remove("input-error");
+        errorElement.classList.remove("text-error");
     } else {
         labelElement.classList.add("text-error");
         inputElement.classList.add("input-error");
+        errorElement.classList.add("text-error");
     }
 }
 
-function validate(year, month, day, validDays) {
-    let validYear = validateValueRange(year, 0, 9999) && validateIsInPast(day, month, year);
-    let validMonth = validateValueRange(month, 0, 11) && validateIsInPast(day, month, year);
-    let validDay = validateValueRange(day, 1, 31) && validateIsInPast(day, month, year) && validateDays(day, month, validDays);
+function validateDay() {
+    const labelDay = document.getElementById("day-label");
+    const errorDay = document.getElementById("day-error");
 
-    applyInputError(labelYear, inputYear, validYear);
-    applyInputError(labelMonth, inputMonth, validMonth);
-    applyInputError(labelDay, inputDay, validDay);
+    let isValid = true;
+    if (inputDay.value == null || inputDay.value == "") {
+        errorDay.innerHTML = "This field is required";
+        isValid = false;
+    }
+    
+    applyInputError(inputDay, labelDay, errorDay, isValid);
+    return isValid;
+}
 
-    return validYear && validMonth && validDay;
+function validateMonth() {
+    const labelMonth = document.getElementById("month-label");
+    const errorMonth = document.getElementById("month-error");
+
+    let isValid = true;
+    if (inputMonth.value == null || inputMonth.value == "") {
+        errorMonth.innerHTML = "This field is required";
+        isValid = false;
+    }
+
+    applyInputError(inputMonth, labelMonth, errorMonth, isValid);
+    return isValid;
+}
+
+function validateYear() {
+    const labelYear = document.getElementById("year-label");
+    const errorYear = document.getElementById("year-error");
+
+    let isValid = true;
+    if (inputYear.value == null || inputYear.value == "") {
+        errorYear.innerHTML = "This field is required";
+        isValid = false;
+    }
+
+    applyInputError(inputYear, labelYear, errorYear, isValid);
+    return isValid;
+}
+
+function isValid() {
+    let isDayValid = validateDay();
+    let isMonthValid = validateMonth();
+    let isYearValid = validateYear();
+    
+    return isDayValid && isMonthValid && isYearValid;
 }
 
 function calculateAge() {
-    let [year, month, day] = [inputYear.value, inputMonth.value - 1, inputDay.value];
+    [year, month, day] = [inputYear.value, inputMonth.value - 1, inputDay.value];
 
-    let feb = year % 4 == 0 ? 29 : 28;
-    let validDays = [31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    feb = year % 4 == 0 ? 29 : 28;
+    validDays = [31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    if (validate(year, month, day, validDays)) {
+    if (isValid()) {
         let currDate = new Date();
         let inputDate = new Date(year, month, day);
         
